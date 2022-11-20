1ee4d6486abd4ee8810f34598ef58f6b6a781547
Daniel Keicher <dnlkch86@gmail.com>
2022-11-20 18:47:42 +0100

    passing props to button, mapping btn array

--- src/features/drum-machine/Button.js
+++ src/features/drum-machine/Button.js
@@ -1,34 +1,34 @@
 import React from "react";
-// import styles from "./Button.module.css";
+import styles from "./Button.module.css";
 import { useDispatch } from "react-redux";
 import { select } from "./drumMachineSlice";
 
-export default function Button() {
+export default function Button(props) {
     const dispatch = useDispatch();
 
     const handleClick = (e) => {
         let btn = document.getElementById(e.target.id);
         dispatch(select(e.target.value));
-        btn.style.border = "3px solid #FF0000";
-        btn.style.backgroundColor = "#FF8080";
-        btn.style.color = "#FF0000";
+        btn.style.border = props.btn.c_border;
+        btn.style.backgroundColor = props.btn.c_bgColor;
+        btn.style.color = props.btn.c_color;
         setTimeout(() => {
             dispatch(select(""));
-            btn.style.border = "3px solid #FFF";
-            btn.style.backgroundColor = "grey";
-            btn.style.color = "#FFF";
+            btn.style.border = props.btn.border;
+            btn.style.backgroundColor = props.btn.bgColor;
+            btn.style.color = props.btn.color;
         }, 500);
     };
 
     return (
         <div>
             <button
-                id="button1"
-                // className={styles.myButton}
-                value={"I'm button #1"}
+                id={props.btn.id}
+                className={styles.myButton}
+                value={props.btn.value}
                 onClick={handleClick}
             >
-                BTN1
+                {props.btn.text}
             </button>
         </div>
     );
--- src/features/drum-machine/DrumMachine.js
+++ src/features/drum-machine/DrumMachine.js
@@ -1,20 +1,36 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { select, showSelected } from "./drumMachineSlice";
+import { useSelector } from "react-redux";
+import { showSelected } from "./drumMachineSlice";
 import styles from "./DrumMachine.module.css";
 import Display from "./Display";
 import Button from "./Button";
 
 export function DrumMachine() {
     const selected = useSelector(showSelected);
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
+
+    const drumPadButtons = [
+        {
+            id: "button1",
+            value: "#1 YOOOO!!!",
+            text: "YOOOO",
+            border: "3px solid #FFF",
+            bgColor: "grey",
+            color: "#FFF",
+            c_border: "3px solid #FF0000",
+            c_bgColor: "#FF8080",
+            c_color: "#FF0000",
+        },
+    ];
 
     return (
         <div id="drum-machine" className={styles.drumMachineContainer}>
             <h1>Ultimate Drum Machine</h1>
             <div className={styles.switch}></div>
             <Display selected={selected} />
-            <Button />
+            {drumPadButtons.map((v) => (
+                <Button btn={v} />
+            ))}
         </div>
     );
 }
